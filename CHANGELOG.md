32aa6669801a81a562bacebf3edef996c0fe9f75
aoisensi <aoisensi@outlook.jp>
2020-03-25 23:50:21 +0900

    Fix library import

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1
+
+- Fix library import
+
 ## 0.1.0
 
 - Initial version, created by Stagehand
--- lib/kokoroio.dart
+++ lib/kokoroio.dart
@@ -2,3 +2,5 @@ library kokoroio;
 
 export 'src/kokoroio.dart';
 
+export 'src/models/access_token.dart';
+export 'src/models/profile.dart';
